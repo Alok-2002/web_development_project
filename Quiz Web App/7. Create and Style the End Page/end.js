20a22519ac905212302315686a2da04a7bad53c8
Alok Sharma <2021eo262al@mitsgwl.ac.in>
2023-05-19 14:09:58 +0530

    Create end.js

---
+++ Quiz Web App/7. Create and Style the End Page/end.js
@@ -0,0 +1,13 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+    e.preventDefault();
+};

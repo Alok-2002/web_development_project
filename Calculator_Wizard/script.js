9d4cb4a006ce94109888ec1267d1e6beb291e1bd
Alok Sharma <2021eo262al@mitsgwl.ac.in>
2023-08-01 14:38:33 +0530

    Create script.js

---
+++ Calculator_Wizard/script.js
@@ -0,0 +1,23 @@
+let outputScreen = document.getElementById('output-Screen')
+
+function display(num){
+    outputScreen.value += num;
+}
+
+function Calculate(){
+    try{
+        outputScreen.value =eval(outputScreen.value);
+    }
+    catch(err)
+    {
+        alert('Invalid')
+    }
+}
+
+function Clear(){
+    outputScreen.value = "";
+}
+
+function del(){
+    outputScreen.value = outputScreen.value.slice(0, -1);
+}
